1f227b0c4de7ca89e2704f3b47c836ecfc41620e
BondiolaAlHorno <110265653+BondiolaAlHorno@users.noreply.github.com>
2025-03-04 20:38:04 -0300

    se añadio un cronometro y la velocidad promedio al terminar

--- app/src/main/java/com/example/running_app/MainActivity.kt
+++ app/src/main/java/com/example/running_app/MainActivity.kt
@@ -14,13 +14,13 @@ import com.google.android.gms.location.LocationServices
 import android.location.Location
 import android.location.LocationManager
 import android.os.Looper
-import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.location.LocationAvailability
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
@@ -63,6 +63,8 @@ class MainActivity : AppCompatActivity() {
         // Inicializar FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        initTimer()
+
         if (!checkLocationPermission()){
             requestLocationPermission()
         }
@@ -73,6 +75,8 @@ class MainActivity : AppCompatActivity() {
                     resetTrackingData()
                     startTracking()
                     binding.playStop.setImageResource(R.drawable.stop)
+                    resetChronometer()
+                    initChronometer()
                     appSwitch=true
                 }
                 else{
@@ -83,6 +87,8 @@ class MainActivity : AppCompatActivity() {
             else{
                 stopTracking()
                 binding.playStop.setImageResource(R.drawable.play)
+                stopChronometer()
+                updateUIavarageSpeed()
                 appSwitch=false
             }
         }
@@ -146,8 +152,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateUI() {
-        binding.speed.text = "Velocidad: ${"%.1f".format(currentSpeed)} km/h"
-        binding.distance.text = "Distancia: ${"%.1f".format(totalDistance/1000)} km"
+        binding.speed.text = "Velocidad: ${"%.2f".format(currentSpeed)} km/h"
+        binding.distance.text = "Distancia: ${"%.2f".format(totalDistance/1000)} km"
     }
 
+    private fun updateUIavarageSpeed(){
+        if ((totalDistance.toInt() != 0) && (elapsedTimeInSeconds!=0)){
+            var avaragespeed = (totalDistance/1000.0)/(elapsedTimeInSeconds.toDouble()/3600.0)
+            binding.speed.text = "Velocidad promedio: ${String.format("%.2f", avaragespeed)} km/h"
+        }
+        else{
+            binding.speed.text = "Velocidad promedio: 0 km/h"
+        }
+
+    }
+
+    private var elapsedTimeInSeconds = 0
+    private lateinit var handler: Handler
+    private lateinit var timerRunnable: Runnable
+
+    private fun initTimer() {
+        handler = Handler(Looper.getMainLooper())
+        timerRunnable = object : Runnable {
+            override fun run() {
+                updateTimerDisplay()
+                elapsedTimeInSeconds++
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+    private fun updateTimerDisplay() {
+        val hours = elapsedTimeInSeconds / 3600
+        val minutes = (elapsedTimeInSeconds % 3600) / 60
+        val seconds = elapsedTimeInSeconds % 60
+        val timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        binding.timeMarker.text = "Tiempo: $timeString"
+    }
+
+    private fun initChronometer(){
+        handler.post(timerRunnable)
+    }
+
+    private fun resetChronometer(){
+        elapsedTimeInSeconds = 0
+    }
+
+    private fun stopChronometer(){
+        handler.removeCallbacks(timerRunnable)
+    }
 }
